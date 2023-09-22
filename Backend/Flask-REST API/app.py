f6bee706bb0fc6c465a0e0c78f742d232d7d1169
MohomedAadil <aadil.mohomed786@gmail.com>
2023-09-22 20:29:55 +0530

    MySQL DB connection

--- Backend/Flask-REST API/app.py
+++ Backend/Flask-REST API/app.py
@@ -1,10 +1,19 @@
 import keras
 import pickle
+import pymysql
 from flask import Flask
 
 app = Flask(__name__)
 
+# MySQL Database Configuration
+db_host = 'localhost'
+db_user = 'root'
+db_password = 'root'
+db_name = 'textdb'
 
+# Initialize the database connection
+db = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+cursor = db.cursor()
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
