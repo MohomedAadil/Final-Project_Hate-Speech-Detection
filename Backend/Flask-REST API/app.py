52fed2d105e09bae6decb1fc628ab3414e7cbe31
MohomedAadil <aadil.mohomed786@gmail.com>
2023-09-22 17:23:36 +0530

    ML model imported into the project

--- Backend/Flask-REST API/app.py
+++ Backend/Flask-REST API/app.py
@@ -1,9 +1,18 @@
+import keras
+import pickle
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+# Load the model and tokenizer
+load_model = keras.models.load_model("hate&abusive_model.h5")
+with open('tokenizer.pickle', 'rb') as handle:
+    load_tokenizer = pickle.load(handle)
 
 if __name__ == '__main__':
     app.run()
