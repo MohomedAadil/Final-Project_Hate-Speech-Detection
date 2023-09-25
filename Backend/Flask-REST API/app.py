63cf9e05ea42abddb09765894c11e958fc51cf65
MohomedAadil <aadil.mohomed786@gmail.com>
2023-09-25 13:02:45 +0530

    changes in flask_cors

--- Backend/Flask-REST API/app.py
+++ Backend/Flask-REST API/app.py
@@ -7,7 +7,7 @@ import pymysql
 from keras.preprocessing import sequence
 from nltk.stem import PorterStemmer
 from flask import Flask, request, jsonify
-from flask import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
