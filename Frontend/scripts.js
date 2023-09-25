521385e89a47e89debedc95bde35684b2c2b63bb
MohomedAadil <aadil.mohomed786@gmail.com>
2023-09-25 12:41:00 +0530

    Function to fetch and display posts

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -9,3 +9,33 @@ const viewAllPostsButton = document.getElementById('viewAllPosts');
 function showError(message) {
     errorContainer.textContent = message;
 }
+
+// Function to fetch and display posts
+async function fetchPosts() {
+    try {
+        const response = await fetch('http://localhost:5000/get_posts');
+        const posts = await response.json();
+
+        postList.innerHTML = '';
+
+        posts.forEach(post => {
+            const li = document.createElement('li');
+            li.className = 'post-item';
+            li.textContent = post.content;
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', () => deletePost(post.id));
+
+            const updateButton = document.createElement('button');
+            updateButton.textContent = 'Update';
+            updateButton.addEventListener('click', () => updatePost(post.id, post.content));
+
+            li.appendChild(deleteButton);
+            li.appendChild(updateButton);
+            postList.appendChild(li);
+        });
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+}
