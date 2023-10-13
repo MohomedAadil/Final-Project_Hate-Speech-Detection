cfa53e71e5274e2454d6999c51e3a95cbce5ad5f
MohomedAadil <aadil.mohomed786@gmail.com>
2023-10-13 19:56:31 +0530

    changes

--- Backend/FlaskRestAPI/app.py
+++ Backend/FlaskRestAPI/app.py
@@ -1,4 +1,4 @@
-import keras
+from tensorflow import keras
 import re
 import os
 import string
@@ -6,7 +6,6 @@ import nltk
 import pickle
 import pymysql
 import emoji
-from keras.models import load_model
 from keras.preprocessing import sequence
 from nltk.stem import PorterStemmer
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
